bfaddb54cacad87fa6484c891e1041384f28c54b
iizaghz <sukmahawaiza@gmail.com>
2026-09-24 17:38:33 +0700

    add Loops Kotlin examples and screenshot for For and While

---
+++ code/06_Loops/ForWhile.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    // For
+    for (number in 1..5) {
+        // number is the iterator and 1..5 is the range
+        print(number)
+    }
+    // 12345
+
+    println()
+
+    // While
+    var cakes = 0
+
+    while (cakes < 3) {
+        println("Eat a cake")
+        cakes++
+    }
+}
