75194d1a8507608831d339a67ba43dda5aebfe6c
iizaghz <sukmahawaiza@gmail.com>
2026-09-24 17:11:24 +0700

    add BasicTypes Kotlin  and screenshot

---
+++ code/02_TipeDataDasar/BasicTypes.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    // Variable dideklarasikan tanpa inisialisasi
+    val d: Int
+
+    // Variable dengan inisialisasi
+    d = 3
+
+    // Variable jenis explicit dan di inisialisasi
+    val e: String = "hello"
+
+    println(d)
+    println(e)
+}
